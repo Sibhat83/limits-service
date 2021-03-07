66bee20f2f1c5f39d7a0a408831c384e075a8f47
Sibhat83 <sid.syntel@gmail.com>
2021-03-07 16:09:31 -0600

    inital

---
+++ src/main/java/com/example/limitsservice/controller/LimitsController.java
@@ -0,0 +1,16 @@
+package com.example.limitsservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.limitsservice.bean.Limits;
+
+@RestController
+public class LimitsController {
+	@GetMapping("/limits")
+	public Limits retrievelimits() {
+		return new Limits(1,1000);
+	}
+	
+
+}
