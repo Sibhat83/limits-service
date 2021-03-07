1337542e51a6e99b40ef666b9f5a2cd15caf08b8
Sibhat83 <sid.syntel@gmail.com>
2021-03-07 17:55:57 -0600

    3rd

--- src/main/java/com/example/limitsservice/controller/LimitsController.java
+++ src/main/java/com/example/limitsservice/controller/LimitsController.java
@@ -1,15 +1,24 @@
 package com.example.limitsservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.limitsservice.bean.Limits;
+import com.example.limitsservice.configuration.Configuration;
 
 @RestController
 public class LimitsController {
+	
+	@Autowired
+	private Configuration configuration;
+	
 	@GetMapping("/limits")
 	public Limits retrievelimits() {
-		return new Limits(1,1000);
+		//return new Limits(1,1000);
+		return new Limits (configuration.getMinimum(),
+							configuration.getMaximum());
 	}
 	//new limits
 
